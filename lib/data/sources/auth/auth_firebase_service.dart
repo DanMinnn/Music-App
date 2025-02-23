64d24b9cce33eb9f1f8a1acfa67bbdf8a18941bb
Dang Minh <minhdangngoc304@gmail.com>
2025-02-23 10:03:47 +0700

    change Appwrite to Firebase

--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,34 +1,32 @@
-import 'package:appwrite/appwrite.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
-import 'package:music/core/configs/appwrite/AppwriteService.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:music/data/models/auth/create_user_req.dart';
 import 'package:music/data/models/auth/signin_user_req.dart';
 
-abstract class AuthAppWriteService {
+abstract class AuthFirebaseService {
   Future<Either> signUp(CreateUserReq createUserReq);
 
   Future<Either> signIn(SigninUserReq signInUserReq);
 }
 
-class AuthAppWriteServiceImpl extends AuthAppWriteService {
-  final AppwriteService _appwriteService = AppwriteService();
-
+class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
   Future<Either> signIn(SigninUserReq signInUserReq) async {
     try {
-      await _appwriteService.account.createEmailPasswordSession(
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: signInUserReq.email,
         password: signInUserReq.password,
       );
 
       return Right('Sign in was successful');
-    } on AppwriteException catch (e) {
+    } on FirebaseAuthException catch (e) {
       String message = '';
 
-      if (e.type == 'user_invalid_credentials') {
-        message = 'Please check the email and password.';
-      } else {
-        message = '${e.type}';
+      if (e.code == 'invalid-email') {
+        message = 'Not user found for that email';
+      } else if (e.code == 'invalid-credential') {
+        message = 'Wrong password provide for that user ';
       }
 
       return Left(message);
@@ -38,20 +36,24 @@ class AuthAppWriteServiceImpl extends AuthAppWriteService {
   @override
   Future<Either> signUp(CreateUserReq createUserReq) async {
     try {
-      await _appwriteService.account.create(
-        userId: ID.unique(),
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: createUserReq.email,
         password: createUserReq.password,
       );
 
+      FirebaseFirestore.instance.collection('User').add({
+        'name': createUserReq.fullName,
+        'email': data.user?.email,
+      });
+
       return Right('Sign up was successful');
-    } on AppwriteException catch (e) {
+    } on FirebaseAuthException catch (e) {
       String message = '';
 
-      if (e.type == 'user_email_already_exists') {
+      if (e.code == 'weak-password') {
+        message = 'The password provided is too week';
+      } else if (e.code == 'email-already') {
         message = 'An account already exists with that email.';
-      } else if (e.type == 'general_argument_invalid') {
-        message = '${e.type}';
       }
       return Left(message);
     }
